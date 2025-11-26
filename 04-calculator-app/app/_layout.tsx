98ef69a217788d1712588d3844ea36325f6b3cc1
Jorge Andres Alvarez Melchor <joralvmel@gmail.com>
2025-11-26 16:22:35 +0100

    feat: implement calculator app layout and update theme colors

--- 04-calculator-app/app/_layout.tsx
+++ 04-calculator-app/app/_layout.tsx
@@ -1,24 +1,25 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {View} from 'react-native';
+import {Slot} from "expo-router";
+import {useFonts} from "expo-font";
+import {Colors} from "@/constants/theme";
+import {StatusBar} from "expo-status-bar";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+const RootLayout = () => {
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    })
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+    if (!loaded) {
+        return null;
+    }
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    return (
+        <View style={{backgroundColor: Colors.background, flex: 1, padding: 30}}>
+            <Slot />
+            <StatusBar style="light" />
+        </View>
+    )
 }
+
+export default RootLayout
---
+++ 04-calculator-app/app/index.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const CalculatorApp = () => {
+    return (
+        <View>
+            <Text style={{fontSize: 40, fontFamily: 'SpaceMono', color:"white"}}>CalculatorApp</Text>
+        </View>
+    )
+}
+
+export default CalculatorApp
