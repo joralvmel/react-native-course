b97d34735cf7cd224e857f4517d2e924a70ab631
Jorge Andres Alvarez Melchor <joralvmel@gmail.com>
2025-11-26 12:28:37 +0100

    feat: create basic counter app

---
+++ 03-counter-app/App.tsx
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {useState} from "react";
+import FAB from "./components/FAB";
+
+export default function App() {
+  const [count, setCount] = useState(10)
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textHuge}>{count}</Text>
+
+      <FAB
+          label={"+1"}
+          onPress={()=>setCount(count+1)}
+          onLongPress={()=>setCount(0)}
+          position={"right"}
+      />
+
+      <FAB
+          label={"Reset"}
+          onPress={()=>setCount(0)}
+          position={"left"}
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textHuge: {
+    fontSize: 48,
+    fontWeight: '100',
+  },
+
+});
