1b05c45ad81f707e32fe3da94b27f786096f6cca
Jorge Andres Alvarez Melchor <joralvmel@gmail.com>
2025-12-10 17:21:07 +0100

    feat: add custom font integration and improve layout with SafeAreaView

--- 05-navigation-app/app/index.tsx
+++ 05-navigation-app/app/index.tsx
@@ -1,10 +1,18 @@
 import {Text, View} from 'react-native';
 
+import {SafeAreaView} from "react-native-safe-area-context";
+
 const App = () => {
     return (
-        <View className="mt-10">
-            <Text className="text-3xl font-bold text-blue-500">App</Text>
-        </View>
+        <SafeAreaView>
+            <View className="mt-6 mx-2.5">
+                <Text className="text-5xl" style={{fontFamily:'WorkSans-Black'}}>App</Text>
+                <Text className="text-4xl font-work-black">App</Text>
+                <Text className="text-3xl font-work-medium">App</Text>
+                <Text className="text-2xl font-work-light">App</Text>
+                <Text className="text-xl">App</Text>
+            </View>
+        </SafeAreaView>
     )
 }
 
