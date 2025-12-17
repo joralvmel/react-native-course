1db8fb83b5132b12cd6fa326ec1b50459601214a
Jorge Andres Alvarez Melchor <joralvmel@gmail.com>
2025-12-17 12:34:46 +0100

    feat: add loading indicator to HomeScreen while fetching movie data

--- 06-movies-app/app/home/index.tsx
+++ 06-movies-app/app/home/index.tsx
@@ -1,15 +1,22 @@
-import {Text, View} from 'react-native';
+import {ActivityIndicator, Text, View} from 'react-native';
 import {useMovies} from "@/presentation/hooks/useMovies";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+    const safeArea = useSafeAreaInsets();
     const { nowPlayingQuery } = useMovies();
 
+    if (nowPlayingQuery.isLoading) {
+        return (
+            <View className='justify-center items-center flex-1'>
+                <ActivityIndicator size={40} color="purple" />
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <Text>HomeScreen</Text>
-            <Text>
-                {JSON.stringify(nowPlayingQuery.data, null, 2)}
-            </Text>
+        <View className='mt-2' style={{paddingTop: safeArea.top}}>
+            <Text className='text-3xl font-bold px-4 mb-2'>HomeScreen</Text>
         </View>
     )
 }
